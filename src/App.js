e0f055a100e2096f32b318d59d9aed6126925fe2
sedx876 <swatkins876@gmail.com>
2020-10-21 04:22:18 -0400

    Added behindthescenes.js and behindthescenes.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import Enter from './components/Enter'
+//import Enter from './components/Enter'
+import "bootswatch/dist/cyborg/bootstrap.min.css"
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import About from './components/About'
+import News from './components/News'
+import BehindTheScenes from './components/BehindTheScenes'
 
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <Enter />
+      {/* <Enter /> */}
+      <Navbar />
+      {/* <Home /> */}
+      {/* <About /> */}
+      {/* <News /> */}
+      <BehindTheScenes/>
+      <Footer />
     </div>
   );
 }
